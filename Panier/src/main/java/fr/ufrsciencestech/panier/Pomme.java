997944de82264934f506d1f975b2229f28ec082f
clement <clement.hadj@outlook.fr>
2021-09-14 15:27:58 +0200

    Mon commit

---
+++ Panier/src/main/java/fr/ufrsciencestech/panier/Pomme.java
@@ -0,0 +1,72 @@
+package fr.ufrsciencestech.panier;
+
+/**
+ *
+ * @author roudet
+ */
+public class Pomme {
+    private double prix;
+    private String origine;
+	
+    public Pomme() 
+    {
+        this.prix = 0.5;  //prix en euros
+        this.origine="Espagne";
+    }
+    
+    public Pomme(double prix, String origine) 
+    {
+	if(prix < 0)
+	    this.prix = -prix;  //une solution possible pour interdire les prix negatifs
+	else
+	    this.prix = prix;
+
+	if(origine.equals(""))
+            this.origine = "Espagne";  //Espagne par défaut
+	else
+            this.origine = origine;   
+    }
+
+    public double getPrix(){
+	return prix;
+    }
+
+    public void setPrix(double prix){
+	this.prix=prix;
+    }
+
+    public String getOrigine(){
+	return origine;
+    }
+ 
+    public void setOrigine(String origine){
+	this.origine=origine;
+    }
+
+    @Override
+    public String toString(){
+        return "Pomme de " + origine + " a " + prix + " euros";
+    }
+
+    @Override
+    public boolean equals(Object o){  //predicat pour tester si 2 Pommes sont equivalentes
+        if(o != null && getClass() == o.getClass()){
+            Pomme or = (Pomme) o;
+            return (prix == or.prix && origine.equals(or.origine));
+        }
+        return false;
+    }
+
+    public boolean isSeedless() {  //predicat indiquant qu'une Pomme a des pepins
+        return false;
+    }
+
+
+    public static void main (String[] args){
+        //Ecrire ici vos tests
+        Pomme unePomme = new Pomme();
+        System.out.println(unePomme.toString());
+        Pomme unePomme2 = new Pomme(10, "france");
+        System.out.println(unePomme2.toString());
+   }
+}
